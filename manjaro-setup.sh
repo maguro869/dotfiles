cf7c828e3b5e8193579bad95775831b884f5542f
maguro869 <50356023+maguro869@users.noreply.github.com>
2020-10-09 14:42:22 +0900

    [とりあえず]Manjaroで最初に動かすScript

---
+++ manjaro-setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#    __  __              _
+#   |  \/  | __ _ _ __  (_) __ _ _ __ ___
+#   | |\/| |/ _` | '_ \ | |/ _` | '__/ _ \
+#   | |  | | (_| | | | || | (_| | | | (_) |
+#   |_|  |_|\__,_|_| |_|/ |\__,_|_|  \___/
+#                     |__/
+
+
+echo "----------INSTALL START----------"
+
+# pacman
+sudo pacman-mirrors --fasttrack
+sudo pacman -Syyu
+
+# ホームディレクトリ英語化
+sudo pacman -S xdg-user-dirs-gtk
+LANG=C xdg-user-dirs-gtk-update
+
+echo -n "mopidyをインストールしますか？(Y/n)"
+read yn
+if [ $yn -gt "Y" ];then
+    sudo pacman -S mopidy mopidy-mpd mopidy-spotify
+
+
+echo "--------INSTALL COMPLETE---------"
+
+exit 0
